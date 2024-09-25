249eb63a2c50c9bad06cbde50584366c8ae3e155
VINIshow1907 <viniciuslucio792@gmail.com>
2024-09-25 14:02:00 -0300

    :sparkles: feat: estrutura das cores criada

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: 'hsl(var(--primary-light))',
+          DEFAULT: 'hsl(var(--primary-default))',
+          dark: 'hsl(var(--primary-dark))',
+        },
+        secondary: {
+          light: 'hsl(var(--secondary-light))',
+          DEFAULT: 'hsl(var(--secondary-default))',
+          dark: 'hsl(var(--secondary-dark))',
+        },
+        neutral: {
+          light: 'hsl(var(--neutral-light))',
+          DEFAULT: 'hsl(var(--neutral-default))',
+          dark: 'hsl(var(--neutral-dark))',
+        },
+        text: {
+          light: 'hsl(var(--text-light))',
+          DEFAULT: 'hsl(var(--text-default))',
+          dark: 'hsl(var(--text-dark))',
+        },
+        background: {
+          light: 'hsl(var(--background-light))',
+          DEFAULT: 'hsl(var(--background-default))',
+          dark: 'hsl(var(--background-dark))',
+        },
+      }
+    },
   },
   plugins: [],
 }
